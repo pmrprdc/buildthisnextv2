33de6c529ad72aeee672bdcbed0cf22ccfc3eda3
Pamir Perdeci <pamirperdeci@gmail.com>
2024-03-21 19:17:09 -0400

    listed all the items in one bracket

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,37 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import styled from 'styled-components';
+
+
+
+const AppDiv = styled.div`
+  background-color: #007bff;
+  color: white;
+  font-size: 1rem;
+  padding: 0.375rem 0.75rem;
+  border: 1px solid #007bff;
+  border-radius: 0.25rem;
+  cursor: pointer;
+  transition: background-color 0.15s ease-in-out;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
+
+
+const items = [
+  "ghostRacr", "Beautiful Robot Written Letters", "Candle Like Focus Trainer","vintage-royal-tees"
+]
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-     <div>
+     <AppDiv>
       <h1>Welcom To Build This NEXT</h1>
       <h3>You can vote on your favorite project below</h3>
 
@@ -24,7 +48,7 @@ function App() {
           Candle Like Focus Trainer For Desktop
         </li>
       </ul>
-     </div>
+     </AppDiv>
     </>
   )
 }
